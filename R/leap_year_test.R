1a6d988724e3379d2ab1eb17a720525b5ce1be8c
user <m.vregelaar@gmail.com>
2016-01-07 15:50:17 +0100

    project whith a function that tests if a year is a leap year

---
+++ R/leap_year_test.R
@@ -0,0 +1,31 @@
+leap_test <- function(year=2009) {
+	# Function to calculate if a year is a leap year
+	# year (numeric) is the year for which the fuction will test if it is as leap year
+
+	# check if year is numeric
+	if(!is.numeric(year)) {
+		stop("argument of class numeric expected")
+	}
+	# check if year is in the period of leapyears
+	else if (year<1752){ 
+		result= paste("the year", year, "is out of the valid range")
+	}
+	
+	# check if the year is a leap year
+	else if (year%%4!=0){
+		result=FALSE			
+	}
+	else if (year%%100!=0){
+		result=TRUE
+	}
+	else if (year%%400!=0){
+		result=FALSE
+	}
+	else {
+		result=TRUE
+		
+		}
+	# print the result
+	print(result)
+}
+
---
+++ leap_year_main.R
@@ -0,0 +1,17 @@
+# Hakken en zagen
+# Mark ten Vregelaar and Jos Goris
+#01/07/2016
+
+#Read R Code from leap_year_test.R, which contains the funstion leap_test
+source("leap_year_test.R")
+
+# test the function with a few different input
+
+leap_test("kaas")
+leap_test(1545)
+leap_test(2000)
+leap_test(1752)
+leap_test(2052)
+leap_test(2001)
+leap_test(1952)
+leap_test(1998)
